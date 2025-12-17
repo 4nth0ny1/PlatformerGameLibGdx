8c3c1a44fc35b8637b1efcaa160290e7aab97ae2
4nth0ny1 <acatullo4@gmail.com>
2025-12-17 00:09:56 -0600

    removed duplicate enemy class from main

--- core/src/main/java/com/anthony/platformer/PlatformerGame.java
+++ core/src/main/java/com/anthony/platformer/PlatformerGame.java
@@ -19,31 +19,6 @@ import java.util.List;
 
 public class PlatformerGame extends ApplicationAdapter {
 
-    private static class Enemy {
-        public float x;
-        public float y;
-
-        public float width;
-        public float height;
-
-        public boolean facingRight;
-        public boolean isMoving;
-
-        public float stateTimeSeconds;
-
-        public Enemy(float x, float y, float width, float height) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-
-            this.facingRight = true;
-            this.isMoving = true;
-
-            this.stateTimeSeconds = 0f;
-        }
-    }
-
     private SpriteBatch batch;
 
     private Texture playerSheetTexture;
